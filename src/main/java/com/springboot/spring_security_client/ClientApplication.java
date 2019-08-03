fda00f8128cd9a87cef1d47ec09ef0fae5375b3e
MJCoderMJCoder <598157378@qq.com>
2019-08-03 09:37:23 +0800

    Securing a Web Application： https://spring.io/guides/gs/securing-web/
    Spring Boot and OAuth2：
    https://spring.io/guides/tutorials/spring-boot-oauth2/

--- src/main/java/com/springboot/spring_security_client/ClientApplication.java
+++ src/main/java/com/springboot/spring_security_client/ClientApplication.java
@@ -12,6 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
+
+/**
+ * @SpringBootApplication is a convenience annotation that adds all of the following:
+ * @Configuration tags the class as a source of bean definitions for the application context.
+ * @EnableAutoConfiguration tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings.
+ * <p>
+ * Normally you would add @EnableWebMvc for a Spring MVC app, but Spring Boot adds it automatically when it sees spring-webmvc on the classpath.
+ * This flags the application as a web application and activates key behaviors such as setting up a DispatcherServlet.
+ * @ComponentScan tells Spring to look for other components, configurations, and services in the hello package, allowing it to find the controllers.
+ * <p>
+ * The main() method uses Spring Boot’s SpringApplication.run() method to launch an application.
+ */
 @EnableAutoConfiguration
 @Configuration
 @EnableOAuth2Sso
